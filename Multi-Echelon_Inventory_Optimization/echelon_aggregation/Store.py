709fc822b11e6884c26b989e6b65a4c607473ff0
ASHWEEN RAJ <ashween.sridhar@aciesglobal.com>
2025-06-16 12:56:40 +0530

    changed df name

--- Multi-Echelon_Inventory_Optimization/echelon_aggregation/Store.py
+++ Multi-Echelon_Inventory_Optimization/echelon_aggregation/Store.py
@@ -3,29 +3,29 @@ from operations import operations
 from Preassumptions import CODE_MAP,HOLDING_COST,LEAD_TIME,ORDERING_COST
 
 def store_data(df):
-    forecasted_df=df
-    for i in range(len(forecasted_df)):
+    echelon_df=df
+    for i in range(len(echelon_df)):
 
-        ordering_cost = ORDERING_COST[CODE_MAP[forecasted_df.loc[i,"Store"]]]
-        holding_cost = HOLDING_COST[CODE_MAP[forecasted_df.loc[i,"Store"]]]
-        lead_time = LEAD_TIME[CODE_MAP[forecasted_df.loc[i,"Store"]]]
+        ordering_cost = ORDERING_COST[CODE_MAP[echelon_df.loc[i,"Store"]]]
+        holding_cost = HOLDING_COST[CODE_MAP[echelon_df.loc[i,"Store"]]]
+        lead_time = LEAD_TIME[CODE_MAP[echelon_df.loc[i,"Store"]]]
         
         key = next((k for k, v in CODE_MAP.items() if v == "DC"), None)
-        forecasted_df.loc[i,"DC"]=key
-        # forecasted_df.loc[i,"monthly_eoq"]=operations.eoq_manual(ordering_cost,holding_cost,forecasted_df.loc[i,"Store_Monthly_Demand"])
-        forecasted_df.loc[i,"monthly_eoq"]=operations.EOQ(ordering_cost,holding_cost,forecasted_df.loc[i,"Store_Monthly_Demand"])
+        echelon_df.loc[i,"DC"]=key
+        # echelon_df.loc[i,"monthly_eoq"]=operations.eoq_manual(ordering_cost,holding_cost,echelon_df.loc[i,"Store_Monthly_Demand"])
+        echelon_df.loc[i,"monthly_eoq"]=operations.EOQ(ordering_cost,holding_cost,echelon_df.loc[i,"Store_Monthly_Demand"])
 
-        forecasted_df.loc[i,'cycle_time']=operations.cycle_time(forecasted_df.loc[i,'monthly_eoq'],forecasted_df.loc[i,'Store_Monthly_Demand'])
-        forecasted_df.loc[i,"cycle_time_in_days"]=operations.cycle_time_month_to_days(forecasted_df.loc[i,"cycle_time"])
-        forecasted_df.loc[i,"cycle_time_in_hr"]=operations.cycle_time_days_to_hrs(forecasted_df.loc[i,"cycle_time_in_days"])
-        forecasted_df.loc[i,"full_cycles_in_lead_time"]=operations.full_cycle_in_lead_time(lead_time,forecasted_df.loc[i,"cycle_time"])
+        echelon_df.loc[i,'cycle_time']=operations.cycle_time(echelon_df.loc[i,'monthly_eoq'],echelon_df.loc[i,'Store_Monthly_Demand'])
+        echelon_df.loc[i,"cycle_time_in_days"]=operations.cycle_time_month_to_days(echelon_df.loc[i,"cycle_time"])
+        echelon_df.loc[i,"cycle_time_in_hr"]=operations.cycle_time_days_to_hrs(echelon_df.loc[i,"cycle_time_in_days"])
+        echelon_df.loc[i,"full_cycles_in_lead_time"]=operations.full_cycle_in_lead_time(lead_time,echelon_df.loc[i,"cycle_time"])
     
-        forecasted_df.loc[i,"effective_lead_time"]=operations.effective_lead_time(lead_time,forecasted_df.loc[i,"full_cycles_in_lead_time"],forecasted_df.loc[i,"cycle_time"])
-        forecasted_df.loc[i,"reorder_point"]=operations.reorder_point(forecasted_df.loc[i,"Store_Monthly_Demand"],forecasted_df.loc[i,"effective_lead_time"])
+        echelon_df.loc[i,"effective_lead_time"]=operations.effective_lead_time(lead_time,echelon_df.loc[i,"full_cycles_in_lead_time"],echelon_df.loc[i,"cycle_time"])
+        echelon_df.loc[i,"reorder_point"]=operations.reorder_point(echelon_df.loc[i,"Store_Monthly_Demand"],echelon_df.loc[i,"effective_lead_time"])
 
-        dc_val = int(forecasted_df.loc[i, "DC"]) 
-        year_val = forecasted_df.loc[i, "Year"]
-        month_val = forecasted_df.loc[i, "Month"]
+        dc_val = int(echelon_df.loc[i, "DC"]) 
+        year_val = echelon_df.loc[i, "Year"]
+        month_val = echelon_df.loc[i, "Month"]
         
-        forecasted_df.loc[i, "key"] = f"{dc_val}_{year_val}_{month_val}"
-    return forecasted_df
+        echelon_df.loc[i, "key"] = f"{dc_val}_{year_val}_{month_val}"
+    return echelon_df
--- Multi-Echelon_Inventory_Optimization/echelon_aggregation/Warehouse.py
+++ Multi-Echelon_Inventory_Optimization/echelon_aggregation/Warehouse.py
@@ -3,32 +3,32 @@ from operations import operations
 from Preassumptions import CODE_MAP,HOLDING_COST,LEAD_TIME,ORDERING_COST
 
 def warehouse_data(df):
-    forecasted_df=df
-    for i in range(len(forecasted_df)):
+    echelon_df=df
+    for i in range(len(echelon_df)):
 
-        ordering_cost = ORDERING_COST[CODE_MAP[forecasted_df.loc[i,"Warehouse"]]]
-        holding_cost = HOLDING_COST[CODE_MAP[forecasted_df.loc[i,"Warehouse"]]]
-        lead_time = LEAD_TIME[CODE_MAP[forecasted_df.loc[i,"Warehouse"]]]
+        ordering_cost = ORDERING_COST[CODE_MAP[echelon_df.loc[i,"Warehouse"]]]
+        holding_cost = HOLDING_COST[CODE_MAP[echelon_df.loc[i,"Warehouse"]]]
+        lead_time = LEAD_TIME[CODE_MAP[echelon_df.loc[i,"Warehouse"]]]
 
 
-        # forecasted_df.loc[i,"monthly_eoq"]=operations.eoq_manual(ordering_cost,holding_cost,forecasted_df.loc[i,"Warehouse_Monthly_Demand"])
-        forecasted_df.loc[i,"monthly_eoq"]=operations.EOQ(ordering_cost,holding_cost,forecasted_df.loc[i,"Warehouse_Monthly_Demand"])
+        # echelon_df.loc[i,"monthly_eoq"]=operations.eoq_manual(ordering_cost,holding_cost,echelon_df.loc[i,"Warehouse_Monthly_Demand"])
+        echelon_df.loc[i,"monthly_eoq"]=operations.EOQ(ordering_cost,holding_cost,echelon_df.loc[i,"Warehouse_Monthly_Demand"])
 
-        forecasted_df.loc[i,'cycle_time']=operations.cycle_time(forecasted_df.loc[i,'monthly_eoq'],forecasted_df.loc[i,'Warehouse_Monthly_Demand'])
-        forecasted_df.loc[i,"cycle_time_in_days"]=operations.cycle_time_month_to_days(forecasted_df.loc[i,"cycle_time"])
-        forecasted_df.loc[i,"cycle_time_in_hr"]=operations.cycle_time_days_to_hrs(forecasted_df.loc[i,"cycle_time_in_days"])
-        forecasted_df.loc[i,"full_cycles_in_lead_time"]=operations.full_cycle_in_lead_time(lead_time,forecasted_df.loc[i,"cycle_time"])
+        echelon_df.loc[i,'cycle_time']=operations.cycle_time(echelon_df.loc[i,'monthly_eoq'],echelon_df.loc[i,'Warehouse_Monthly_Demand'])
+        echelon_df.loc[i,"cycle_time_in_days"]=operations.cycle_time_month_to_days(echelon_df.loc[i,"cycle_time"])
+        echelon_df.loc[i,"cycle_time_in_hr"]=operations.cycle_time_days_to_hrs(echelon_df.loc[i,"cycle_time_in_days"])
+        echelon_df.loc[i,"full_cycles_in_lead_time"]=operations.full_cycle_in_lead_time(lead_time,echelon_df.loc[i,"cycle_time"])
     
-        forecasted_df.loc[i,"effective_lead_time"]=operations.effective_lead_time(lead_time,forecasted_df.loc[i,"full_cycles_in_lead_time"],forecasted_df.loc[i,"cycle_time"])
-        forecasted_df.loc[i,"reorder_point"]=operations.reorder_point(forecasted_df.loc[i,"Warehouse_Monthly_Demand"],forecasted_df.loc[i,"effective_lead_time"])
+        echelon_df.loc[i,"effective_lead_time"]=operations.effective_lead_time(lead_time,echelon_df.loc[i,"full_cycles_in_lead_time"],echelon_df.loc[i,"cycle_time"])
+        echelon_df.loc[i,"reorder_point"]=operations.reorder_point(echelon_df.loc[i,"Warehouse_Monthly_Demand"],echelon_df.loc[i,"effective_lead_time"])
 
-        forecasted_df["DC"] = forecasted_df["DC"].fillna(0).astype(float).astype(int)
+        echelon_df["DC"] = echelon_df["DC"].fillna(0).astype(float).astype(int)
 
-        dc_val = int(forecasted_df.loc[i, "DC"]) 
-        year_val = forecasted_df.loc[i, "Year"]
-        month_val = forecasted_df.loc[i, "Month"]
+        dc_val = int(echelon_df.loc[i, "DC"]) 
+        year_val = echelon_df.loc[i, "Year"]
+        month_val = echelon_df.loc[i, "Month"]
         
-        forecasted_df.loc[i, "key"] = f"{dc_val}_{year_val}_{month_val}"
+        echelon_df.loc[i, "key"] = f"{dc_val}_{year_val}_{month_val}"
 
-    return forecasted_df
+    return echelon_df
 
