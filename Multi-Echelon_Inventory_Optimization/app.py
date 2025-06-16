4c92d9b6d23785d00b0f9ffd600fd20f38f57c2b
Acies-Rishikesh <rishikesh.balaji@aciesglobal.com>
2025-06-16 12:24:46 +0530

    Removed unused code and improved readability

--- Multi-Echelon_Inventory_Optimization/app.py
+++ Multi-Echelon_Inventory_Optimization/app.py
@@ -10,30 +10,30 @@ from schedules import store_schedule
 from schedules import warehouse_schedule
 from Preassumptions import STORE_SCHEDULE,WAREHOUSE_SCHEDULE
 
-csv_path = r"C:\Users\DELL\Desktop\project-1 R&S\inventory_optimization\Multi-Echelon_Inventory_Optimization\data\Sample_2.csv"
+csv_path = r"C:\Users\RISHIKESH\Desktop\inventory_optimization\Multi-Echelon_Inventory_Optimization\data\Sample_2.csv"
 
 
 df = load_file_as_dataframe(csv_path, date_col="Time.[Week]")
 
-store_df = aggregate_store_monthly(df, date_col='TimeWeek', value_col='Actual')
-warehouse_df = aggregate_warehouse_monthly(df, date_col='TimeWeek', value_col='Actual')
-dc_df = aggregate_dc_monthly(df, date_col='TimeWeek', value_col='Actual')
-
-
 
 #
+store_df = aggregate_store_monthly(df, date_col='TimeWeek', value_col='Actual')
 store_df.to_excel("./Multi-Echelon_Inventory_Optimization/output_data/monthly_demand/store_aggregated_monthly_demand.xlsx", index=False, engine='openpyxl')
+
 store_demand_df=store_data(store_df)
 store_demand_df.to_excel("./Multi-Echelon_Inventory_Optimization/output_data/calculated_metrics/store_monthly_metrics.xlsx",index=False,engine='openpyxl')
 
+warehouse_df = aggregate_warehouse_monthly(df, date_col='TimeWeek', value_col='Actual')
 warehouse_df.to_excel("./Multi-Echelon_Inventory_Optimization/output_data/monthly_demand/warehouse_aggregated_monthly_demand.xlsx", index=False, engine='openpyxl')
+
 warehouse_demand_df=warehouse_data(warehouse_df)
 warehouse_demand_df.to_excel("./Multi-Echelon_Inventory_Optimization/output_data/calculated_metrics/warehouse_monthly_metrics.xlsx",index=False,engine='openpyxl')
 
+dc_df = aggregate_dc_monthly(df, date_col='TimeWeek', value_col='Actual')
 dc_df.to_excel("./Multi-Echelon_Inventory_Optimization/output_data/monthly_demand/dc_aggregated_monthly_demand.xlsx", index=False, engine='openpyxl')
+
 dc_demand_df=dc_data(dc_df)
 dc_demand_df.to_excel("./Multi-Echelon_Inventory_Optimization/output_data/calculated_metrics/dc_monthly_metrics.xlsx",index=False,engine='openpyxl')
-
 
 
 
